8c239b88a2f37ed08dde8d59a3edd8c50ff01736
Anupam-75 <167006555+Anupam-75@users.noreply.github.com>
2025-07-03 00:17:56 +0530

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -23,14 +23,3 @@ const transactionSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Transaction', transactionSchema);
-const mongoose = require('mongoose');
-
-const transactionSchema = new mongoose.Schema({
-  bookId: { type: mongoose.Schema.Types.ObjectId, ref: 'Book' },
-  bookName: { type: String, required: true },  // Add this
-  type: { type: String, enum: ['Add', 'Sold'], required: true },
-  quantity: { type: Number, required: true },  // Rename from quantityChanged
-  date: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Transaction', transactionSchema);
